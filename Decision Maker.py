fe1c0196caf47f94d74ae547fa7f3bc7991fe802
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2022-11-19 18:55:59 +0800

    Added initial Dictionary

--- Decision Maker.py
+++ Decision Maker.py
@@ -1,1 +1,11 @@
 # Mark john M. Raymundo CoE 2-2 Data Structure and Algorithms
+# Instructions: Create an interesting/exciting python project/program taking inspiration from a youtube video. Create a demo and present an idea on where you can apply what you created.
+# Idea: An app that helps make you decisions. Video link: https://www.youtube.com/watch?v=_xf1TMs0ysk from Tina Huang
+# Reason for choosing: A game I currently play has a gameplay feature of a crafting system, I want to create an app that would help me make a decision based on a few factors.
+# Taking in user input
+# Name of resource/item // Ore, Wood, Fiber, Stone, Hide //, Tier + Enchantment // T2.0 - T8.3 //, Factors that will affect the decision // Proft, SellVolume, DailyLimit, BuyingVolume //
+ResourceDictionary = {}
+ResourceTierDictionary = {Factor : Proft, SellVolume, DailyLimit, BuyingVolume}
+
+# Proccessing of data
+# Displaying of result
